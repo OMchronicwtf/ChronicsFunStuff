35f42af89fde220cf4b16738b121e9df40feb381
OMchronicwtf <140353421+OMchronicwtf@users.noreply.github.com>
2024-11-04 02:47:45 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,57 +1,49 @@
 // ==SE_module==
-// name: virtual_pet
-// displayName: Virtual Pet
-// description: A Script that allows users to feed a virtual pet on startup.
+// name: random_joke
+// displayName: Random Joke Generator
+// description: A Script That Provides Random Jokes to Users
 // version: 1.0
 // author: Your Name
 // ==/SE_module==
 
-var config = require("config");
-var im = require("interface-manager");
+(function () {
+  'use strict';
 
-var settingsContext = {
-    events: [],
-};
+  var im = require("interface-manager");
+  var jokes = [
+    "Why did the scarecrow win an award? Because he was outstanding in his field!",
+    "I told my computer I needed a break, and now it won't stop sending me beach wallpapers.",
+    "Why don’t skeletons fight each other? They don’t have the guts.",
+    "What do you call fake spaghetti? An impasta!",
+    "Why did the bicycle fall over? Because it was two-tired!"
+  ];
 
-var petHunger = 5; // Pet's hunger level
-
-function createPetToolBoxUI() {
-    settingsContext.events.push({
-        start: function (builder) {
-            builder.row(function (builder) {
-                builder.button("Feed Pet", function () {
-                    feedPet();
-                });
-            });
-        },
-    });
-}
-
-function feedPet() {
-    if (petHunger > 0) {
-        petHunger--;
-        longToast("Yum! Your pet is fed!");
+  function displayMessage(message) {
+    console.log(message);
+    if (typeof shortToast === "function") {
+      shortToast(message);
     } else {
-        longToast("Your pet is not hungry right now!");
+      console.warn("shortToast is not available. Message:", message);
     }
-}
+  }
 
-module.onSnapMainActivityCreate = activity => {
-    createPetToolBoxUI();
-}
+  function getRandomJoke() {
+    const randomIndex = Math.floor(Math.random() * jokes.length);
+    return jokes[randomIndex];
+  }
 
-function createInterface() {
-    createPetToolBoxUI();
-}
-
-function start(_) {
-    createInterface();
-}
-
-start();
-
-im.create("settings" /* EnumUI.SETTINGS */, function (builder, args) {
-    settingsContext.events.forEach(function (event) {
-        event.start(builder, args);
+  function createJokeToolboxUI() {
+    im.create("jokeToolbox", function (builder) {
+      builder.button("Get a Random Joke", function() {
+        const joke = getRandomJoke();
+        displayMessage(joke);
+      });
     });
-});
+  }
+
+  function start() {
+    createJokeToolboxUI();
+  }
+
+  start();
+})();
